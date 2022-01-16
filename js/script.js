556bae3cdc35472577f3d8e70b10f4e0f4a0f6e7
danielmuthama <daniel.muthama@student.moringaschool.com>
2022-01-17 02:30:26 +0300

    updating add pizza section

--- js/script.js
+++ js/script.js
@@ -76,4 +76,55 @@ function GetPizza (name,size,crust,topping,total){
     
       total = price + c_price + topping_value;
       let checkoutTotal = 0;
+      checkoutTotal = checkoutTotal + total;
+
+      $("#pizzaname").html($(".name option:selected").val());
+      $("#pizzasize").html( $("#size option:selected").val());
+      $("#pizzacrust").html($("#crust option:selected").val());
+      $("#pizzatopping").html(pTopping.join(", "));
+      $("#totals").html(total);
+
+      $("button.addPizza").click(function(){
+        let pName = $(".name option:selected").val();
+        let pSize = $("#size option:selected").val();
+        let pCrust = $("#crust option:selected").val();
+        let pTopping = []; 
+    
+        switch(pSize){
+            case "0":
+              price= 0;
+              break;
+            case 'large':
+              price = 1200;
+              break;
+            case "medium":
+              price = 900;
+              break;
+            case "small":
+              price = 600;
+            default:
+              console.log("error");
+          }
+      
+          switch(pCrust){
+            case "0":
+              c_price = 0;
+              break;
+            case "Crispy":
+              c_price = 400;
+              break;
+            case "Stuffed":
+              c_price = 300;
+              break;
+            case "Gluten-free":
+              c_price = 350;
+              break;
+            case "Flatbread":
+              c_price = 350;
+            case "Focaccia":
+              c_price = 250;
+              break;
+            default:
+              console.log("error");
+        }
     
